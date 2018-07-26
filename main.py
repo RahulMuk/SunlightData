c0dc19601f6010a40e0aac7211245ea88eeb8135
Mukherjee, Rahul <Rahul.Mukherjee@startribune.com>
2018-07-26 14:08:39 -0500

    Added option to get selected date and time for data

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python
+import calendar
+monthFirst = 6
+dayFirst = 21
+monthLast = 9 + 1
+dayLast = 22
+year = 2018
 
 import urllib2
 from bs4 import BeautifulSoup
 
 data = []
 
-for i in range(1, 15): 
-	quote_page = 'http://aa.usno.navy.mil/rstt/onedaytable?ID=AA&year=2018&month=7&day=' + format(i) + '&state=MN&place=Minneapolis'
-	page = urllib2.urlopen(quote_page)
-	soup = BeautifulSoup(page, 'html.parser')
-	tag = soup.findAll('td')
-	time = tag[3].text
-	date = soup.find('td', attrs={'class':'tdAleft'}).text
-	data.append((time, date))
+for i in range(monthFirst, monthLast): 
+	if (i == monthLast): 
+		days = dayLast
+	else:
+		days = calendar.monthrange(year,i)[1] + 1
+	if (i == monthFirst):
+		first = dayFirst
+	else:
+		first = 1
+	for j in range(first, days): 
+		quote_page = 'http://aa.usno.navy.mil/rstt/onedaytable?ID=AA&year=' + format(year) +'&month='+ format(i) +'&day=' + format(j) + '&state=MN&place=Minneapolis'
+		page = urllib2.urlopen(quote_page)
+		soup = BeautifulSoup(page, 'html.parser')
+		tag = soup.findAll('td')
+		CT = tag[3].text
+		SR = tag[5].text
+		ST = tag[7].text
+		SS = tag[9].text
+		ECT = tag[11].text
+		date = soup.find('td', attrs={'class':'tdAleft'}).text
+		data.append((date,CT,SR,ST,SS,ECT))
 
 import csv
 from datetime import datetime
 
 with open('index.csv', 'a') as csv_file:
  writer = csv.writer(csv_file)
- writer.writerow(['time', 'date'])
- for time, date in data:
- 	writer.writerow([time, date])
+ writer.writerow(['date','Civil Twilight','Sunrise','Sun transit','Sunset','End civil twilight'])
+ for date,CT,SR,ST,SS,ECT in data:
+ 	writer.writerow([date,CT,SR,ST,SS,ECT])
